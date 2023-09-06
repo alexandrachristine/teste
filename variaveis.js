915061986090cd6e9144d5dbd834d2d7ae2a4404
alexandrachristine <alexandrachristine027@gmail.com>
2023-09-06 12:36:39 -0300

    first commit

---
+++ variaveis.js
@@ -0,0 +1,81 @@
+console.log("oi");
+let total = 10;
+
+// usar console.log para mostrar na tela
+
+// console.log("ddddd");
+
+// ou
+
+// let strX = "aaaa"
+
+// console.log(strX);
+
+//
+
+
+
+
+
+
+
+// crie uma variavel e atribua o seu nome
+let name = "Alexandra";
+console.log(name);
+
+
+
+
+
+// crie uma variavel e atribua o nome de alguem
+let nome = "Reinaldo";
+console.log(nome);
+
+
+
+
+// compare se a primeira variavel é igual a segunda
+
+let nota = 6;
+if (nota <= 5) {
+    console.log("reprovado");
+} else if (nota > 5 && nota < 7) {
+    console.log("recuperação");
+} else {
+    console.log("aprovado");
+}
+
+let valorTotal = 0; //numero ou float
+let valor = 0; //numero ou float
+let nomeUsuario = ""; //string ou texto
+let meuArray = new Array();
+meuArray = ("lelelele", "reinaldo");
+
+valorTotal = valor1 + valor2;
+
+console.log(valorTotal)
+console.log(valorTotal)
+console.log(valorTotal)
+console.log(valorTotal)
+console.log(valorTotal)
+
+//Operadores
+//atribuição =
+//igual== ou ===
+//desigualde !=
+//
+
+let v1 = 1;
+let v2 = 2;
+let vTrue = 0;
+
+if (v1 != v2) {
+    console.log("é diferente ");
+} else {
+    console.log("é igual");
+
+}
+console.log(typeof v1);
+console.log(typeof v2);
+console.log
+console.log
